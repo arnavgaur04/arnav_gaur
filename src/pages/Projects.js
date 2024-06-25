6506e2dccee923176c8804b2bc77409ca7c62257
arnavgaur04 <arnavgaur04@gmail.com>
2024-06-25 18:47:41 +0530

    fixing tech stack

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,23 +8,25 @@ const Projects = () => {
                     <div className="tech left">Node.js</div>
                     <div className="tech">React</div>
                     <div className="tech">Django</div>
+                    <div className="tech">Python</div>
                 </span>
                 <span>
                     <div className="tech left">Node.js</div>
                     <div className="tech">React</div>
                     <div className="tech">Django</div>
+                    <div className="tech">Python</div>
                 </span>
             </div>        
             <div className="project_holder_inside project_holder_inside_two">
                 <span>
                     <div className="tech left">Firebase</div>
                     <div className="tech">My SQL</div>
-                    <div className="tech">Express.js</div>
+                    <div className="tech">MongoDB</div>
                 </span>
                 <span>
                     <div className="tech left">Firebase</div>
                     <div className="tech">My SQL</div>
-                    <div className="tech">Express.js</div>
+                    <div className="tech">MongoDB</div>
                 </span>
             </div>        
         </div>
